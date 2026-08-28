04388d21881a4b232d41abcfd510567c95f69c77
prabuwijaya267-web <prabuwijaya267@gmail.com>
2026-08-28 22:12:20 +0700

    update:dewa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Dewasufa</title>
+    <title>Dewaprabu</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
